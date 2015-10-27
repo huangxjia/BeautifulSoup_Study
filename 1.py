cf4dfcd639f258b62a5811811a5cd5ad2f633fa5
jiajia <982789611@qq.com>
2015-10-27 16:37:39 +0800

    second commit

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@ __author__ = 'Administrator'
 
 import urllib2
 from bs4 import BeautifulSoup
+import codecs
 
 url = 'http://www.bloomberg.com/asia'
 
@@ -9,4 +10,5 @@ url = 'http://www.bloomberg.com/asia'
 if __name__ == "__main__":
     bg = urllib2.urlopen(url).read()
     soup = BeautifulSoup(bg)
-#    print soup.prettify().encode('utf-8')
+#   print soup.prettify().encode('utf-8')
+#   soup.find(), soup.find_all('a'), soup.find_all(re.compile('t'))
